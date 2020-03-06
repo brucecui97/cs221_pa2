8a233a5207dec565f82715a41f8fd8378d08fac7
UBC Student <brucecui@annacis.students.cs.ubc.ca>
2020-03-06 11:29:28 -0800

    add border color picker, not sure if right

--- borderColorPicker.cpp
+++ borderColorPicker.cpp
@@ -3,9 +3,21 @@
 borderColorPicker::borderColorPicker(unsigned int borderSize, HSLAPixel fillColor, PNG &img, double tolerance)
 {
     /* your code here */
+    this->borderSize = borderSize; // Number of pixels to use for border
+    this->fillColor = fillColor;     // Color used for the border
+    this->img = img;                 // Image used in flood fill algorithm
+    this->tolerance = tolerance;        // Tolerance used in flood fill algorithm
 }
 
 HSLAPixel borderColorPicker::operator()(point p)
 {
     /* your code here */
+    if (p.x>=img.width()-borderSize || p.x<=borderSize || p.y>=img.height()-borderSize || p.y<=borderSize){
+        return fillColor;
+    }
+
+    else{
+        return *img.getPixel(p.x,p.y);
+    }
+
 }
